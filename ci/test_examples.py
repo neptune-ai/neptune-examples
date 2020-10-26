b5d5e848bf61644b8fe3286e6b1a9ff32f68e507
Piotr Jander <pjander2@gmail.com>
2020-10-26 14:32:03 +0100

    Fix

--- ci/test_examples.py
+++ ci/test_examples.py
@@ -13,7 +13,7 @@ if os.name == 'nt': # if OS is Windows
     #
     # ERROR: Could not install packages due to an EnvironmentError: [WinError 5] Access is denied: 'c:\\hostedtoolcache\\windows\\python\\3.6.8\\x64\\lib\\site-packages\\~umpy\\.libs\\libopenblas.NOIJJG62EMASZI6NYURL6JBKM4EVBGM7.gfortran-win_amd64.dll'
     # Consider using the `--user` option or check the permissions.
-    excluded_files.extend(glob('product-tours/how-it-works/tests/*.py', recursive=True))
+    excluded_files.extend(glob('product-tours\\how-it-works\\tests\\*.py', recursive=True))
 
 @pytest.mark.parametrize("filename", [f for f in test_files if f not in excluded_files])
 def test_examples(filename):
