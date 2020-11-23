dae9d3b08a99971d7396d0ea699d6e1115d14003
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2020-11-23 16:07:25 +0100

    Added lgb to the optuna tests

--- integrations/optuna/tests/Neptune-Optuna_upgraded_libs.py
+++ integrations/optuna/tests/Neptune-Optuna_upgraded_libs.py
@@ -4,9 +4,9 @@
 
 ## Install dependencies
 
-get_ipython().system(" pip install --quiet optuna==2.3.0 neptune-client==0.4.125 neptune-contrib['monitoring']==0.24.9")
+get_ipython().system(" pip install --quiet optuna==2.3.0 lightgbm==3.1.0 neptune-client==0.4.125 neptune-contrib['monitoring']==0.24.9")
 
-get_ipython().system(" pip install --quiet optuna neptune-client neptune-contrib['monitoring'] --upgrade")
+get_ipython().system(" pip install --quiet optuna lightgbm neptune-client neptune-contrib['monitoring'] --upgrade")
 
 ## Import libraries
 
