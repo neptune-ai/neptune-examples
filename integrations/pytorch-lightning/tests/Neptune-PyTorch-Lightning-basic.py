b7e7004a3039049689dba9a6ba7ddab8d90dbf3d
Kamil A. Kaczmarek <kamil.kaczmarek@neptune.ai>
2020-11-24 22:38:25 +0100

    Version bump (#21)
    * bumped pl to 1.0.7
    
    * bumped neptune-client to 0.4.126
    
    * bumped neptune-contrib to 0.24.9
    
    * Added lgb to the optuna tests
    
    * fix
    
    * Added manual workflow trigger
    
    * dropped manual, moved to separate branch
    
    * Added manual.yml to run form UI
    
    * ci fix

--- integrations/pytorch-lightning/tests/Neptune-PyTorch-Lightning-basic.py
+++ integrations/pytorch-lightning/tests/Neptune-PyTorch-Lightning-basic.py
@@ -4,7 +4,7 @@
 
 ## Install dependencies
 
-get_ipython().system(' pip install pytorch-lightning==1.0.0 neptune-client==0.4.123 torch==1.6.0 torchvision==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html')
+get_ipython().system(' pip install pytorch-lightning==1.0.7 neptune-client==0.4.126 torch==1.6.0 torchvision==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html')
 
 # Step 1: Import Libraries
 
