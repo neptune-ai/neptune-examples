d494d40bbc32b80164a4ba373a2a1dbb22b43a49
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2020-12-15 12:18:23 +0100

    Updated sklearn example

--- integrations/sklearn/tests/Neptune-Scikit-learn.py
+++ integrations/sklearn/tests/Neptune-Scikit-learn.py
@@ -4,7 +4,7 @@
 
 ## Install dependencies
 
-get_ipython().system(' pip install scikit-learn==0.23.2 neptune-client==0.4.126 neptune-contrib==0.24.9')
+get_ipython().system(' pip install scikit-learn==0.23.2 neptune-client==0.4.129 neptune-contrib==0.25.0')
 
 # Scikit-learn regression
 
@@ -171,14 +171,12 @@ exp.stop()
 import neptune
 from sklearn.cluster import KMeans
 from sklearn.datasets import load_boston, load_digits, load_linnerud, make_blobs
-from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, ExtraTreesRegressor,     GradientBoostingRegressor, AdaBoostClassifier, RandomForestClassifier, GradientBoostingClassifier,     BaggingClassifier, ExtraTreesClassifier, VotingClassifier
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestClassifier
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.linear_model import Ridge, LinearRegression, MultiTaskElasticNet, MultiTaskLasso,     LogisticRegression, Perceptron, RidgeClassifier
+from sklearn.linear_model import Ridge, LinearRegression, LogisticRegression, Perceptron, RidgeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.multioutput import MultiOutputRegressor
-from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier, ExtraTreeClassifier
 
 from neptunecontrib.monitoring.sklearn import log_regressor_summary, log_classifier_summary,     log_kmeans_clustering_summary
 
@@ -186,50 +184,24 @@ neptune.init('shared/sklearn-integration',
              api_token='ANONYMOUS')
 
 def run_regressors_single_output():
-    # Data
     X, y = load_boston(return_X_y=True)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=28743)
 
-    # Regressors, training, logging
-    tag = 'single-output'
-
-    abr = AdaBoostRegressor()
-    train_regressor(abr, 'AdaBoostRegressor', tag, X_train, X_test, y_train, y_test)
-
-    etr = ExtraTreesRegressor()
-    train_regressor(etr, 'ExtraTreesRegressor', tag, X_train, X_test, y_train, y_test)
-
     gpr = GaussianProcessRegressor()
-    train_regressor(gpr, 'GaussianProcessRegressor', tag, X_train, X_test, y_train, y_test)
+    train_regressor(gpr, 'GaussianProcessRegressor', 'single-output', X_train, X_test, y_train, y_test)
 
     gbr = GradientBoostingRegressor()
-    train_regressor(gbr, 'GradientBoostingRegressor', tag, X_train, X_test, y_train, y_test)
+    train_regressor(gbr, 'GradientBoostingRegressor', 'single-output', X_train, X_test, y_train, y_test)
 
     lr = LinearRegression()
-    train_regressor(lr, 'LinearRegression', tag, X_train, X_test, y_train, y_test)
-
-    rfr = RandomForestRegressor()
-    train_regressor(rfr, 'RandomForestRegressor', tag, X_train, X_test, y_train, y_test)
-
-    r = Ridge()
-    train_regressor(r, 'Ridge', tag, X_train, X_test, y_train, y_test)
+    train_regressor(lr, 'LinearRegression', 'single-output', X_train, X_test, y_train, y_test)
 
 def run_regressors_multi_output():
-    # Data
     X, y = load_linnerud(return_X_y=True)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
-    # Regressors, training, logging
-    tag = 'multi-output'
-
     mor = MultiOutputRegressor(Ridge())
-    train_regressor(mor, 'MultiOutputRegressor', tag, X_train, X_test, y_train, y_test)
-
-    mten = MultiTaskElasticNet()
-    train_regressor(mten, 'MultiTaskElasticNet', tag, X_train, X_test, y_train, y_test)
-
-    mtl = MultiTaskLasso()
-    train_regressor(mtl, 'MultiTaskLasso', tag, X_train, X_test, y_train, y_test)
+    train_regressor(mor, 'MultiOutputRegressor', 'multi-output', X_train, X_test, y_train, y_test)
 
 def train_regressor(reg, name, tag, X_train, X_test, y_train, y_test):
     neptune.create_experiment(tags=[name, tag, 'regression'])
@@ -238,48 +210,23 @@ def train_regressor(reg, name, tag, X_train, X_test, y_train, y_test):
     neptune.stop()
 
 def run_classifiers():
-    # Data
     X, y = load_digits(return_X_y=True)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=28743)
 
-    # Classifier, training, logging
-    tag = 'single-output'
-
-    abc = AdaBoostClassifier()
-    train_classifier(abc, 'AdaBoostClassifier', tag, X_train, X_test, y_train, y_test)
-
     rfc = RandomForestClassifier()
-    train_classifier(rfc, 'RandomForestClassifier', tag, X_train, X_test, y_train, y_test)
-
-    bc = BaggingClassifier()
-    train_classifier(bc, 'BaggingClassifier', tag, X_train, X_test, y_train, y_test)
-
-    dtc = DecisionTreeClassifier()
-    train_classifier(dtc, 'DecisionTreeClassifier', tag, X_train, X_test, y_train, y_test)
-
-    etc = ExtraTreeClassifier()
-    train_classifier(etc, 'ExtraTreeClassifier', tag, X_train, X_test, y_train, y_test)
-
-    etsc = ExtraTreesClassifier()
-    train_classifier(etsc, 'ExtraTreesClassifier', tag, X_train, X_test, y_train, y_test)
-
-    gnb = GaussianNB()
-    train_classifier(gnb, 'GaussianNB', tag, X_train, X_test, y_train, y_test)
-
-    gbc = GradientBoostingClassifier()
-    train_classifier(gbc, 'GradientBoostingClassifier', tag, X_train, X_test, y_train, y_test)
+    train_classifier(rfc, 'RandomForestClassifier', 'single-output', X_train, X_test, y_train, y_test)
 
     knc = KNeighborsClassifier()
-    train_classifier(knc, 'KNeighborsClassifier', tag, X_train, X_test, y_train, y_test)
+    train_classifier(knc, 'KNeighborsClassifier', 'single-output', X_train, X_test, y_train, y_test)
 
     lr = LogisticRegression()
-    train_classifier(lr, 'LogisticRegression', tag, X_train, X_test, y_train, y_test)
+    train_classifier(lr, 'LogisticRegression', 'single-output', X_train, X_test, y_train, y_test)
 
     p = Perceptron()
-    train_classifier(p, 'Perceptron', tag, X_train, X_test, y_train, y_test)
+    train_classifier(p, 'Perceptron', 'single-output', X_train, X_test, y_train, y_test)
 
     rc = RidgeClassifier()
-    train_classifier(rc, 'RidgeClassifier', tag, X_train, X_test, y_train, y_test)
+    train_classifier(rc, 'RidgeClassifier', 'single-output', X_train, X_test, y_train, y_test)
 
 def train_classifier(clf, name, tag, X_train, X_test, y_train, y_test):
     neptune.create_experiment(tags=[name, tag, 'classifier'])
@@ -288,14 +235,9 @@ def train_classifier(clf, name, tag, X_train, X_test, y_train, y_test):
     neptune.stop()
 
 def run_clustering():
-    # Data
     X, y = make_blobs(n_samples=579, n_features=17, centers=7, random_state=28743)
-
-    # Clustering, training, logging
-    tag = 'clustering'
-
     km = KMeans()
-    train_clustering(km, 'KMeans', tag, X)
+    train_clustering(km, 'KMeans', 'clustering', X)
 
 def train_clustering(model, name, tag, data):
     neptune.create_experiment(tags=[name, tag, 'clustering'])
