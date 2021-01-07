562cb62f9fa9e97d98fd2f5c1cda13a06de8ddca
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2021-01-07 16:16:30 +0100

    Updates, according to the review.

--- integrations/sklearn/docs/Neptune-Scikit-learn.py
+++ integrations/sklearn/docs/Neptune-Scikit-learn.py
@@ -6,13 +6,11 @@
 
 # Scikit-learn regression
 
-## Step 1: Import libraries
+## Step 1: Create and fit random forest regressor
 
 from sklearn.datasets import load_boston
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-
-## Step 2: Create and fit random forest regressor
 
 rfr = RandomForestRegressor(n_estimators=70, max_depth=7, min_samples_split=3)
 
@@ -21,19 +19,24 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random
 
 rfr.fit(X_train, y_train)
 
-## Step 3: Create Neptune experiment and log regressor summary
+## Step 2: Initialize Neptune
 
 import neptune
+
+neptune.init('shared/sklearn-integration', api_token='ANONYMOUS')
+
+## Step 3: Create an Experiment
+
+neptune.create_experiment(name='regression-example',
+                          tags=['RandomForestRegressor', 'regression'])
+
+## Step 4: Log regressor summary
+
 from neptunecontrib.monitoring.sklearn import log_regressor_summary
 
-neptune.init('shared/sklearn-integration',
-             api_token='ANONYMOUS')
-exp = neptune.create_experiment(name='regression-example',
-                                tags=['RandomForestRegressor', 'regression'])
+log_regressor_summary(rfr, X_train, X_test, y_train, y_test)
 
-log_regressor_summary(rfr, X_train, X_test, y_train, y_test, experiment=exp)
-
-## Step 4: Stop Neptune experiment after logging summary
+## Step 5: Stop Neptune experiment after logging summary
 
 exp.stop()
 
@@ -41,13 +44,11 @@ exp.stop()
 
 # Scikit-learn classification
 
-## Step 1: Import libraries
+## Step 1: Create and fit gradient boosting classifier
 
 from sklearn.datasets import load_digits
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
-
-## Step 2: Create and fit gradient boosting classifier
 
 gbc = GradientBoostingClassifier(n_estimators=120, learning_rate=0.12, min_samples_split=3, min_samples_leaf=2)
 
@@ -56,19 +57,24 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random
 
 gbc.fit(X_train, y_train)
 
-## Step 3: Create Neptune experiment and log regressor summary
+## Step 2: Initialize Neptune
 
 import neptune
+
+neptune.init('shared/sklearn-integration', api_token='ANONYMOUS')
+
+## Step 3: Create an Experiment
+
+neptune.create_experiment(name='classification-example',
+                          tags=['GradientBoostingClassifier', 'classification'])
+
+## Step 4: Log classifier summary
+
 from neptunecontrib.monitoring.sklearn import log_classifier_summary
 
-neptune.init('shared/sklearn-integration',
-             api_token='ANONYMOUS')
-exp = neptune.create_experiment(name='classification-example',
-                                tags=['GradientBoostingClassifier', 'classification'])
+log_classifier_summary(gbc, X_train, X_test, y_train, y_test)
 
-log_classifier_summary(gbc, X_train, X_test, y_train, y_test, experiment=exp)
-
-## Step 4: Stop Neptune experiment after logging summary
+## Step 5: Stop Neptune experiment after logging summary
 
 exp.stop()
 
@@ -76,31 +82,77 @@ exp.stop()
 
 # Scikit-learn KMeans clustering
 
-## Step 1: Import libraries
+## Step 1: Create KMeans object and example data
 
 from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
-
-## Step 2: Create KMeans object and example data
 
 km = KMeans(n_init=11, max_iter=270)
 
 X, y = make_blobs(n_samples=579, n_features=17, centers=7, random_state=28743)
 
-## Step 3: Create Neptune experiment and log KMeans clustering summary
+## Step 2: Initialize Neptune
 
 import neptune
+
+neptune.init('shared/sklearn-integration', api_token='ANONYMOUS')
+
+## Step 3: Create an Experiment
+
+neptune.create_experiment(name='clustering-example', tags=['KMeans', 'clustering'])
+
+## Step 4: Log KMeans clustering summary
+
 from neptunecontrib.monitoring.sklearn import log_kmeans_clustering_summary
 
-neptune.init('shared/sklearn-integration',
-             api_token='ANONYMOUS')
-exp = neptune.create_experiment(name='clustering-example',
-                                tags=['KMeans', 'clustering'])
+log_kmeans_clustering_summary(km, X, n_clusters=17)
 
-log_kmeans_clustering_summary(km, X, n_clusters=17, experiment=exp)
-
-## Step 4: Stop Neptune experiment after logging summary
+## Step 5: Stop Neptune experiment after logging summary
 
 exp.stop()
 
 ## Explore Results
+
+# Other logging options
+
+## Before you start: create and fit gradient boosting classifier
+
+from sklearn.ensemble import RandomForestClassifier
+
+rfc = RandomForestClassifier()
+
+X, y = load_digits(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=28743)
+
+rfc.fit(X_train, y_train)
+
+## Log estimator parameters
+
+from neptunecontrib.monitoring.sklearn import log_estimator_params
+
+neptune.create_experiment(name='estimator-params')
+
+log_estimator_params(rfc) # log estimator parameters here
+
+neptune.stop()
+
+## Log model
+
+from neptunecontrib.monitoring.sklearn import log_pickled_model
+
+neptune.create_experiment(name='pickled-model')
+
+log_pickled_model(rfc, 'my_model') # log pickled model parameters here.
+                                   # path to file in the Neptune artifacts is ``model/<my_model>``.
+
+neptune.stop()
+
+## Log confusion matrix
+
+from neptunecontrib.monitoring.sklearn import log_confusion_matrix_chart
+
+neptune.create_experiment(name='confusion-matrix-chart')
+
+log_confusion_matrix_chart(rfc, X_train, X_test, y_train, y_test) # log confusion matrix chart
+
+neptune.stop()
--- integrations/sklearn/tests/Neptune-Scikit-learn.py
+++ integrations/sklearn/tests/Neptune-Scikit-learn.py
@@ -4,17 +4,15 @@
 
 ## Install dependencies
 
-get_ipython().system(' pip install scikit-learn==0.23.2 neptune-client==0.4.129 neptune-contrib[monitoring]==0.25.0')
+get_ipython().system(' pip install --quiet scikit-learn==0.23.2 neptune-client==0.4.129 neptune-contrib[monitoring]==0.25.0')
 
 # Scikit-learn regression
 
-## Step 1: Import libraries
+## Step 1: Create and fit random forest regressor
 
 from sklearn.datasets import load_boston
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-
-## Step 2: Create and fit random forest regressor
 
 rfr = RandomForestRegressor(n_estimators=70, max_depth=7, min_samples_split=3)
 
@@ -23,19 +21,26 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random
 
 rfr.fit(X_train, y_train)
 
-## Step 3: Create Neptune experiment and log regressor summary
+## Step 2: Initialize Neptune
 
 import neptune
+
+neptune.init('shared/sklearn-integration', api_token='ANONYMOUS')
+
+## Step 3: Create an Experiment
+
+neptune.create_experiment(name='regression-example',
+                          tags=['RandomForestRegressor', 'regression'])
+
+## Step 4: Log regressor summary
+
 from neptunecontrib.monitoring.sklearn import log_regressor_summary
 
-neptune.init('shared/sklearn-integration',
-             api_token='ANONYMOUS')
-exp = neptune.create_experiment(name='regression-example',
-                                tags=['RandomForestRegressor', 'regression'])
-
-log_regressor_summary(rfr, X_train, X_test, y_train, y_test, experiment=exp)
+log_regressor_summary(rfr, X_train, X_test, y_train, y_test)
 
 # tests
+exp = neptune.get_experiment()
+
 # check logs
 correct_logs_set = {'evs_test_sklearn', 'me_test_sklearn', 'mae_test_sklearn', 'r2_test_sklearn', 'charts_sklearn'}
 from_exp_logs = set(exp.get_logs().keys())
@@ -44,21 +49,7 @@ assert correct_logs_set == from_exp_logs, '{} - incorrect logs'.format(exp)
 # check parameters
 assert set(exp.get_properties().keys()) == set(rfr.get_params().keys()), '{} parameters do not match'.format(exp)
 
-# check estimator
-import joblib
-import os
-import tempfile
-
-import numpy as np
-
-with tempfile.TemporaryDirectory() as d:
-    exp.download_artifact('model/estimator.skl', d)
-    full_path = os.path.join(d, 'estimator.skl')
-    model = joblib.load(full_path)
-
-assert np.array_equal(model.predict(X_test), rfr.predict(X_test)), '{} estimator error'.format(exp)
-
-## Step 4: Stop Neptune experiment after logging summary
+## Step 5: Stop Neptune experiment after logging summary
 
 exp.stop()
 
@@ -66,13 +57,11 @@ exp.stop()
 
 # Scikit-learn classification
 
-## Step 1: Import libraries
+## Step 1: Create and fit gradient boosting classifier
 
 from sklearn.datasets import load_digits
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
-
-## Step 2: Create and fit gradient boosting classifier
 
 gbc = GradientBoostingClassifier(n_estimators=120, learning_rate=0.12, min_samples_split=3, min_samples_leaf=2)
 
@@ -81,19 +70,26 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random
 
 gbc.fit(X_train, y_train)
 
-## Step 3: Create Neptune experiment and log regressor summary
+## Step 2: Initialize Neptune
 
 import neptune
+
+neptune.init('shared/sklearn-integration', api_token='ANONYMOUS')
+
+## Step 3: Create an Experiment
+
+neptune.create_experiment(name='classification-example',
+                          tags=['GradientBoostingClassifier', 'classification'])
+
+## Step 4: Log classifier summary
+
 from neptunecontrib.monitoring.sklearn import log_classifier_summary
 
-neptune.init('shared/sklearn-integration',
-             api_token='ANONYMOUS')
-exp = neptune.create_experiment(name='classification-example',
-                                tags=['GradientBoostingClassifier', 'classification'])
-
-log_classifier_summary(gbc, X_train, X_test, y_train, y_test, experiment=exp)
+log_classifier_summary(gbc, X_train, X_test, y_train, y_test)
 
 # tests
+exp = neptune.get_experiment()
+
 # check logs
 correct_logs_set = {'charts_sklearn'}
 for name in ['precision', 'recall', 'fbeta_score', 'support']:
@@ -105,21 +101,7 @@ assert correct_logs_set == from_exp_logs, '{} - incorrect logs'.format(exp)
 # check parameters
 assert set(exp.get_properties().keys()) == set(gbc.get_params().keys()), '{} parameters do not match'.format(exp)
 
-# check estimator
-import joblib
-import os
-import tempfile
-
-import numpy as np
-
-with tempfile.TemporaryDirectory() as d:
-    exp.download_artifact('model/estimator.skl', d)
-    full_path = os.path.join(d, 'estimator.skl')
-    model = joblib.load(full_path)
-
-assert np.array_equal(model.predict_proba(X_test), gbc.predict_proba(X_test)), '{} estimator error'.format(exp)
-
-## Step 4: Stop Neptune experiment after logging summary
+## Step 5: Stop Neptune experiment after logging summary
 
 exp.stop()
 
@@ -127,30 +109,34 @@ exp.stop()
 
 # Scikit-learn KMeans clustering
 
-## Step 1: Import libraries
+## Step 1: Create KMeans object and example data
 
 from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
-
-## Step 2: Create KMeans object and example data
 
 km = KMeans(n_init=11, max_iter=270)
 
 X, y = make_blobs(n_samples=579, n_features=17, centers=7, random_state=28743)
 
-## Step 3: Create Neptune experiment and log KMeans clustering summary
+## Step 2: Initialize Neptune
 
 import neptune
+
+neptune.init('shared/sklearn-integration', api_token='ANONYMOUS')
+
+## Step 3: Create an Experiment
+
+neptune.create_experiment(name='clustering-example', tags=['KMeans', 'clustering'])
+
+## Step 4: Log KMeans clustering summary
+
 from neptunecontrib.monitoring.sklearn import log_kmeans_clustering_summary
 
-neptune.init('shared/sklearn-integration',
-             api_token='ANONYMOUS')
-exp = neptune.create_experiment(name='clustering-example',
-                                tags=['KMeans', 'clustering'])
-
-log_kmeans_clustering_summary(km, X, n_clusters=17, experiment=exp)
+log_kmeans_clustering_summary(km, X, n_clusters=17)
 
 # tests
+exp = neptune.get_experiment()
+
 # check logs
 assert list(exp.get_logs().keys()) == ['charts_sklearn'], '{} - incorrect logs'.format(exp)
 
@@ -160,91 +146,52 @@ assert X.shape[0] == len(km.labels_), '{} incorrect number of cluster labels'.fo
 # check parameters
 assert set(exp.get_properties().keys()) == set(km.get_params().keys()), '{} parameters do not match'.format(exp)
 
-## Step 4: Stop Neptune experiment after logging summary
+## Step 5: Stop Neptune experiment after logging summary
 
 exp.stop()
 
 ## Explore Results
 
-# tests
-# run example regression, classification and clustering jobs
-import neptune
-from sklearn.cluster import KMeans
-from sklearn.datasets import load_boston, load_digits, load_linnerud, make_blobs
-from sklearn.ensemble import GradientBoostingRegressor, RandomForestClassifier
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.linear_model import Ridge, LinearRegression, LogisticRegression, Perceptron, RidgeClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.multioutput import MultiOutputRegressor
-from sklearn.neighbors import KNeighborsClassifier
+# Other logging options
 
-from neptunecontrib.monitoring.sklearn import log_regressor_summary, log_classifier_summary,     log_kmeans_clustering_summary
+## Before you start: create and fit gradient boosting classifier
 
-neptune.init('shared/sklearn-integration',
-             api_token='ANONYMOUS')
+from sklearn.ensemble import RandomForestClassifier
 
-def run_regressors_single_output():
-    X, y = load_boston(return_X_y=True)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=28743)
+rfc = RandomForestClassifier()
 
-    gpr = GaussianProcessRegressor()
-    train_regressor(gpr, 'GaussianProcessRegressor', 'single-output', X_train, X_test, y_train, y_test)
+X, y = load_digits(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=28743)
 
-    gbr = GradientBoostingRegressor()
-    train_regressor(gbr, 'GradientBoostingRegressor', 'single-output', X_train, X_test, y_train, y_test)
+rfc.fit(X_train, y_train)
 
-    lr = LinearRegression()
-    train_regressor(lr, 'LinearRegression', 'single-output', X_train, X_test, y_train, y_test)
+## Log estimator parameters
 
-def run_regressors_multi_output():
-    X, y = load_linnerud(return_X_y=True)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+from neptunecontrib.monitoring.sklearn import log_estimator_params
 
-    mor = MultiOutputRegressor(Ridge())
-    train_regressor(mor, 'MultiOutputRegressor', 'multi-output', X_train, X_test, y_train, y_test)
+neptune.create_experiment(name='estimator-params')
 
-def train_regressor(reg, name, tag, X_train, X_test, y_train, y_test):
-    neptune.create_experiment(tags=[name, tag, 'regression'])
-    reg.fit(X_train, y_train)
-    log_regressor_summary(reg, X_train, X_test, y_train, y_test)
-    neptune.stop()
+log_estimator_params(rfc) # log estimator parameters here
 
-def run_classifiers():
-    X, y = load_digits(return_X_y=True)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=28743)
+neptune.stop()
 
-    rfc = RandomForestClassifier()
-    train_classifier(rfc, 'RandomForestClassifier', 'single-output', X_train, X_test, y_train, y_test)
+## Log model
 
-    knc = KNeighborsClassifier()
-    train_classifier(knc, 'KNeighborsClassifier', 'single-output', X_train, X_test, y_train, y_test)
+from neptunecontrib.monitoring.sklearn import log_pickled_model
 
-    lr = LogisticRegression()
-    train_classifier(lr, 'LogisticRegression', 'single-output', X_train, X_test, y_train, y_test)
+neptune.create_experiment(name='pickled-model')
 
-    p = Perceptron()
-    train_classifier(p, 'Perceptron', 'single-output', X_train, X_test, y_train, y_test)
+log_pickled_model(rfc, 'my_model') # log pickled model parameters here.
+                                   # path to file in the Neptune artifacts is ``model/<my_model>``.
 
-    rc = RidgeClassifier()
-    train_classifier(rc, 'RidgeClassifier', 'single-output', X_train, X_test, y_train, y_test)
+neptune.stop()
 
-def train_classifier(clf, name, tag, X_train, X_test, y_train, y_test):
-    neptune.create_experiment(tags=[name, tag, 'classifier'])
-    clf.fit(X_train, y_train)
-    log_classifier_summary(clf, X_train, X_test, y_train, y_test)
-    neptune.stop()
+## Log confusion matrix
 
-def run_clustering():
-    X, y = make_blobs(n_samples=579, n_features=17, centers=7, random_state=28743)
-    km = KMeans()
-    train_clustering(km, 'KMeans', 'clustering', X)
+from neptunecontrib.monitoring.sklearn import log_confusion_matrix_chart
 
-def train_clustering(model, name, tag, data):
-    neptune.create_experiment(tags=[name, tag, 'clustering'])
-    log_kmeans_clustering_summary(model, X)
-    neptune.stop()
+neptune.create_experiment(name='confusion-matrix-chart')
 
-run_regressors_single_output()
-run_regressors_multi_output()
-run_classifiers()
-run_clustering()
+log_confusion_matrix_chart(rfc, X_train, X_test, y_train, y_test) # log confusion matrix chart
+
+neptune.stop()
