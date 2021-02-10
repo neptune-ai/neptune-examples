55aa40ec95a50b32133928e870bd0520e3f7d238
Szymon Kuklewicz <szymon.kuklewicz@neptune.ai>
2021-02-10 20:57:58 +0100

    Add debug pip freeze to Skopt notebook

--- integrations/skopt/tests/Neptune-Skopt_upgraded_libs.py
+++ integrations/skopt/tests/Neptune-Skopt_upgraded_libs.py
@@ -5,8 +5,10 @@
 ## Install dependencies
 
 get_ipython().system(' pip install --quiet lightgbm==2.2.3 scikit-optimize==0.8.1 neptune-client==0.4.132 neptune-contrib[monitoring]==0.25.0')
+get_ipython().system(' pip freeze')
 
 get_ipython().system(' pip install --quiet scikit-optimize neptune-client neptune-contrib[monitoring] --upgrade')
+get_ipython().system(' pip freeze')
 
 ## Create a sample objective function for skopt
 
